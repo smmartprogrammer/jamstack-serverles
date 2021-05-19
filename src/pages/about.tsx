c1bce2280cf13128ffb5e23b097fc3d89dfcf2a8
Raghunathji <partabrajp@gmail.com>
2021-05-19 22:20:04 +0500

    updating git

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/header';
 import Layout from '../components/Layout';
+import styles from './about.module.css';
 
 export default function About() {
   return (
     <Layout headerTitle="About page header">
       <div>About Page</div>
-      <div>SEcond div</div>
+      <div className={styles.myTitle}>Second div</div>
     </Layout>
   );
 }
